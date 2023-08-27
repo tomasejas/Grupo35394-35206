eeed3b1a907ffa1692fbdab2e3b6d2b8f74684d6
tomasejas <tomasejas1810@gmail.com>
2023-08-27 14:20:02 -0300

    asdf

--- region.hs
+++ region.hs
@@ -1,7 +1,10 @@
 module Region ( Region, newR, foundR, linkR, tunelR, pathR, linksForR, connectedR, linkedR, delayR, availableCapacityForR, usedCapacityForR )
    where
 
+import link
+
 data Region = Reg [City] [Link] [Tunel]
+
 newR :: Region
 newR = Reg [] [] []
 
@@ -9,6 +12,8 @@ foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
 foundR (Reg cities links tunnels) city = Reg (city : cities) links tunnels
 
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
+linkR Reg [] [] [] 
+
 tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
 connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
 linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
